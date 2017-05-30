333255a8c0a9772f9d7732827fb2b5df1285f279
yuzumone <yuzumone@outlook.com>
2017-05-30 14:54:07 +0900

    Enable to work:sparkles:

--- app/src/main/kotlin/net/expandable/sample/MainActivity.kt
+++ app/src/main/kotlin/net/expandable/sample/MainActivity.kt
@@ -1,12 +1,30 @@
 package net.expandable.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import net.expandable.ExpandableTextView
+import net.expandable.OnExpandableClickListener
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById(R.id.text) as ExpandableTextView
+        textView.text = getString(R.string.long_text)
+        textView.setOnExpandableClickListener(object : OnExpandableClickListener{
+            override fun expand(view: ExpandableTextView) {
+                showToast("Expand")
+            }
+
+            override fun unexpand(view: ExpandableTextView) {
+                showToast("Unexpand")
+            }
+        })
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
--- expandabletextview/src/main/kotlin/net/expandable/ExpandableTextView.kt
+++ expandabletextview/src/main/kotlin/net/expandable/ExpandableTextView.kt
@@ -1,10 +1,14 @@
 package net.expandable
 
 import android.content.Context
+import android.text.TextUtils
 import android.util.AttributeSet
 import android.widget.TextView
 
 class ExpandableTextView : TextView {
+
+    private var listener: OnExpandableClickListener? = null
+    private var isExpand: Boolean = false
 
     constructor(context: Context?) :
             this(context, null)
@@ -13,5 +17,43 @@ class ExpandableTextView : TextView {
             this(context, attrs, 0)
 
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
-            super(context, attrs, defStyleAttr)
+            super(context, attrs, defStyleAttr) {
+        init(context!!, attrs!!, defStyleAttr)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet, defStyleAttr: Int) {
+        isClickable = true
+        val array = context.obtainStyledAttributes(attrs, R.styleable.ExpandableTextView, defStyleAttr, 0)
+        isExpand = array.getBoolean(R.styleable.ExpandableTextView_isExpand, false)
+        setExpand(isExpand)
+        if (ellipsize == null) {
+            ellipsize = TextUtils.TruncateAt.END
+        }
+        array.recycle()
+    }
+
+    fun setExpand(expand: Boolean) {
+        isExpand = expand
+        if (expand) {
+            setHorizontallyScrolling(false)
+        } else {
+            setHorizontallyScrolling(true)
+        }
+    }
+
+    fun setOnExpandableClickListener(listener: OnExpandableClickListener) {
+        this.listener = listener
+    }
+
+    override fun performClick(): Boolean {
+        setExpand(!isExpand)
+        if (listener != null) {
+            if (isExpand) {
+                listener!!.expand(this)
+            } else {
+                listener!!.unexpand (this)
+            }
+        }
+        return super.performClick()
+    }
 }
