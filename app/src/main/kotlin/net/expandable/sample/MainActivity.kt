86b7607be2cb4fb6a9dbbca0dde2addafd8bad44
yuzumone <yuzumone@outlook.com>
2020-03-05 09:08:40 +0900

    Migrate to androidx

--- app/src/main/kotlin/net/expandable/sample/MainActivity.kt
+++ app/src/main/kotlin/net/expandable/sample/MainActivity.kt
@@ -1,7 +1,7 @@
 package net.expandable.sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import net.expandable.ExpandableTextView
 
